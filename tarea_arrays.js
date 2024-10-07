da67bc21497799a0793873b59ef5cc984ed5f4a8
Hypatia Garcia <Beatrizmaciel28005@gmail.com>
2024-10-08 00:52:32 +0200

    Update tarea_arrays.js
    corrigiendo error en ejercicio 3

--- tarea_arrays.js
+++ tarea_arrays.js
@@ -28,7 +28,6 @@ sumarPares(num)
 // 3. Buscar en Arrays
 // 3.1 Escribe una función llamada `palabrasTerminanConA` que reciba un array de palabras y devuelva `true` si todas terminan con "a", o `false` si al menos una no lo hace.
 function palabrasTerminanConA(palabras) {
-    function palabrasTerminanConA(palabras) {
         for(let i = 0; i< palabras.length; i++){
             if(!palabras[i].endsWith("a")) {
                 return false
@@ -36,7 +35,7 @@ function palabrasTerminanConA(palabras) {
         }
         return true
     }
-}
+
 
 const palabras = ['casa', 'silla', 'mesa']
 console.log(palabrasTerminanConA(palabras))
